72d33a7ffa15570f6f7c687fd86ac35178d1ba5c
Mikael <dev.kov.mtl@gmail.com>
2022-10-27 11:12:09 -0400

    Client - Login structure and style

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,30 @@
 import "./login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello world.</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex,
+            consectetur delectus incidunt corporis quod doloremque rem vero eius
+            ullam ipsa!
+          </p>
+          <span>Don't have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
