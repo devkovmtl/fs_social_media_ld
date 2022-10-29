6195865933361f9b16b022ef8a8b3b92852b08ef
Mikael <dev.kov.mtl@gmail.com>
2022-10-29 14:47:36 -0400

    Auth context provider

---
+++ client/src/context/authContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = () => {
+    setCurrentUser({ id: 1, name: "John Doe" });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
