4a2a71b0b150c965a8a6a47decd5f0ad377feb32
Mikael <dev.kov.mtl@gmail.com>
2022-10-31 10:50:55 -0400

    Client - Post component layotu and style

---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import Comments from "../comments/Comments";
+
+import "./post.scss";
+
+const Post = ({ post }) => {
+  const [commentOpen, setCommentOpen] = useState(false);
+  const liked = false;
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.img} alt="" />
+            <div className="details">
+              <Link
+                to={`/profile/${post.userId}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <span className="name">{post.name}</span>
+              </Link>
+              <span className="date">1 min ago</span>
+            </div>
+          </div>
+        </div>
+        <div className="content">
+          <p>{post.description}</p>
+          <img src={post.img} alt="" />
+        </div>
+        <div className="info">
+          <div className="item">
+            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+            12 Likes
+          </div>
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+            <TextsmsOutlinedIcon />
+            12 Comments
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon />
+            Share
+          </div>
+        </div>
+        {commentOpen && <Comments />}
+      </div>
+    </div>
+  );
+};
+
+export default Post;
