84b09b4ff21896dc0acfc46ce21a65272a234a8e
Mikael <dev.kov.mtl@gmail.com>
2022-11-02 13:36:07 -0400

    Client - Register via the client

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -7,14 +7,7 @@ export const AuthContextProvider = ({ children }) => {
     JSON.parse(localStorage.getItem("user")) || null
   );
 
-  const login = () => {
-    setCurrentUser({
-      id: 1,
-      name: "John Doe",
-      profilePic:
-        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    });
-  };
+  const login = () => {};
 
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(currentUser));
