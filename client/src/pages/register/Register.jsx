a7ff5dddfcaec5e3ce9572ab98de2084f14f7e5e
Mikael <dev.kov.mtl@gmail.com>
2022-11-03 14:20:56 -0400

    Client - fixed Register bug

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     try {
       await axios.post("http://localhost:8080/api/v1/auth/register", inputs);
     } catch (error) {
-      setErr(err.response.data);
+      setErr(error.response.data.data);
     }
   };
 
