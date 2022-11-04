a32265aa181a9f3a0a2e0a5f1e2e8a0ae15b558a
Mikael <dev.kov.mtl@gmail.com>
2022-11-04 11:26:28 -0400

    Server - Get all the posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,43 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config();
+
+import { db } from "../connect.js";
+
+const { JWT_SECRET } = process.env;
+
 export const getPosts = (req, res, next) => {
-  res.json({
-    success: true,
-    data: "Get Post",
+  const token = req.cookies.accessToken;
+  console.log("TOKEN", token);
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      data: "Not logged in",
+    });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, userInfo) => {
+    if (err) {
+      return res.status(403).json({
+        success: false,
+        data: "Token is not valid",
+      });
+    }
+
+    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r  ON (p.userId = r.followedUserId AND r.followerUserId) WHERE r.followedUserId = ? OR p.userId = ? ORDER BY p.createdAt DESC`;
+
+    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+      if (err) {
+        return res.status(500).json({
+          success: false,
+          data: "Server Error",
+        });
+      }
+
+      return res.json({
+        success: true,
+        data,
+      });
+    });
   });
 };
