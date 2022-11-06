1712ff18a1c441dddcc7308576c59a1052c32efc
Mikael <dev.kov.mtl@gmail.com>
2022-11-06 12:38:38 -0500

    Server - Upload file with multer

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 // Routes
 import authRoutes from "./routes/auth.js";
@@ -31,6 +32,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// setup multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/v1/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  // console.log(file);
+  // {fieldname, originalname, encoding, mimetype, destination, filename, path, size}
+  res.status(200).json({ success: true, data: file.filename });
+});
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/posts", postRoutes);
