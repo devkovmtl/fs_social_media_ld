636f2b9ce605744b0c842c7e70af1b82c5220312
Mikael <dev.kov.mtl@gmail.com>
2022-11-07 09:28:58 -0500

    Client - Fetch comments and add comment

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,45 +1,89 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import moment from "moment";
 import { AuthContext } from "../../context/authContext";
+import { makeRequest } from "../../axios";
 import "./comments.scss";
 
-const comments = [
-  {
-    id: 1,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-    name: "John Doe",
-    userId: 1,
-    profilePicture:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  },
-  {
-    id: 2,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-    name: "Jane Doe",
-    userId: 2,
-    profilePicture:
-      "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  },
-];
+// const comments = [
+//   {
+//     id: 1,
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+//     name: "John Doe",
+//     userId: 1,
+//     profilePicture:
+//       "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   },
+//   {
+//     id: 2,
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+//     name: "Jane Doe",
+//     userId: 2,
+//     profilePicture:
+//       "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+//   },
+// ];
 
-const Comments = () => {
+const Comments = ({ postId }) => {
+  const [desc, setDesc] = useState("");
   const { currentUser } = useContext(AuthContext);
+
+  const {
+    isLoading,
+    error,
+    data: comments,
+  } = useQuery(["comments"], () =>
+    makeRequest.get("/comments?postId=" + postId).then((res) => res.data.data)
+  );
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newComment) => {
+      return makeRequest.post("/comments", newComment);
+    },
+    {
+      onSuccess: () => {
+        // invalidate and refetch
+        queryClient.invalidateQueries(["comments"]);
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc, postId });
+    setDesc("");
+  };
+
   return (
     <div className="comments">
       <div className="write">
-        <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="Write a comment" />
-        <button>Send</button>
+        <img src={"/upload/" + currentUser.profilePic} alt="" />
+        <input
+          type="text"
+          placeholder="Write a comment"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <button onClick={handleClick}>Send</button>
       </div>
-      {comments.map((comment) => (
-        <div key={comment.id} className="comment">
-          <img src={comment.profilePicture} alt="" />
-          <div className="info">
-            <span>{comment.name}</span>
-            <p>{comment.desc}</p>
+      {error ? (
+        <div>Something went wrong</div>
+      ) : isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        comments.map((comment) => (
+          <div key={comment.id} className="comment">
+            <img src={"/upload/" + comment.profilePicture} alt="" />
+            <div className="info">
+              <span>{comment.name}</span>
+              <p>{comment.desc}</p>
+            </div>
+            <span className="date">{moment(comment.createdAt).fromNow()}</span>
           </div>
-          <span className="date">1 hour ago</span>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
