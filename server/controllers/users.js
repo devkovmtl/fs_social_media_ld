72a499bce42cfe94faa8f372f8f1cdf2e192852f
Mikael <dev.kov.mtl@gmail.com>
2022-11-07 17:03:08 -0500

    Client - Get user profile

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,30 @@
+import jwt from "jsonwebtoken";
+import { db } from "../connect.js";
+
 // Get all users
 export const getUser = (req, res, next) => {
   const { userId } = req.params;
-  // Todo
-  res.json({
-    success: true,
-    data: `Find specific user: ${userId}`,
+  const q = "SELECT * FROM users WHERE id=?";
+
+  db.query(q, [userId], (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        data: "Server error",
+      });
+    }
+
+    if (!data.length) {
+      return res.status(403).json({
+        success: false,
+        data: "No user found",
+      });
+    }
+
+    const { password, ...info } = data[0];
+    return res.status(200).json({
+      success: true,
+      data: info,
+    });
   });
 };
