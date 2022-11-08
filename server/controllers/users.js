938d0e95f5faeb17d5500cd2ad5efafd1b8fc450
Mikael <dev.kov.mtl@gmail.com>
2022-11-08 08:14:52 -0500

    Server - Update User from servber

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import { db } from "../connect.js";
+dotenv.config();
+
+const { JWT_SECRET } = process.env;
 
 // Get all users
 export const getUser = (req, res, next) => {
@@ -26,5 +30,46 @@ export const getUser = (req, res, next) => {
       success: true,
       data: info,
     });
+  });
+};
+
+export const updateUser = (req, res, next) => {
+  const token = req.cookies.accessToken;
+  if (!token) {
+    return res.status(401).json({ success: false, data: "Not logged in!" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, userInfo) => {
+    if (err) {
+      return res.status(403).json({
+        success: false,
+        data: "Token is not valid!",
+      });
+    }
+
+    const q =
+      "UPDATE users SET `name`=?,`city`=?,`website`=?,`profilePic`=?,`coverPic`=? WHERE id=? ";
+
+    db.query(
+      q,
+      [
+        req.body.name,
+        req.body.city,
+        req.body.website,
+        req.body.coverPic,
+        req.body.profilePic,
+        userInfo.id,
+      ],
+      (err, data) => {
+        if (err) {
+          return res.status(500).json({ success: false, data: "Server error" });
+        }
+        if (data.affectedRows > 0) {
+          return res.json({ success: true, data: "Updated!" });
+        }
+        return res
+          .status(403)
+          .json({ success: false, data: "You can update only your post!" });
+      }
+    );
   });
 };
