75e0449ce571fe4bdd528323391b5c8921debb9a
Mikael <dev.kov.mtl@gmail.com>
2022-11-08 11:29:20 -0500

    Server - Update fetch posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,7 +9,8 @@ const { JWT_SECRET } = process.env;
 
 export const getPosts = (req, res, next) => {
   const token = req.cookies.accessToken;
-  console.log("TOKEN", token);
+
+  const userId = req.query.userId;
   if (!token) {
     return res.status(401).json({
       success: false,
@@ -25,9 +26,16 @@ export const getPosts = (req, res, next) => {
       });
     }
 
-    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r  ON (p.userId = r.followedUserId AND r.followerUserId) WHERE r.followedUserId = ? OR p.userId = ? ORDER BY p.createdAt DESC`;
+    const q =
+      userId !== "undefined"
+        ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
+        : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+    LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
+    ORDER BY p.createdAt DESC`;
+    const values =
+      userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
 
-    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+    db.query(q, values, (err, data) => {
       if (err) {
         return res.status(500).json({
           success: false,
